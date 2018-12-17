bc48050b5830a172e92cf5787728634e54c75fa5
Benji216 <45763357+Benji216@users.noreply.github.com>
2018-12-17 16:34:17 +0100

    Hllöchen

---
+++ Schule.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Schule {
+    private String name;
+    private long schulkennzahl;
+    private String schultyp;
+
+    public boolean addPersonal(NichtLehrpersonal personal){
+        boolean ret;
+        return ret;
+    }
+    public Abteilung addAbteilung(String name, String kuerzel){
+
+    }
+    public boolean setDirektor(Lehrer lehrer){
+        boolean ret;
+        return ret;
+    }
+    public long getSchulkennzahl(){
+        return schulkennzahl;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getSchultyp(){
+        return schultyp;
+    }
+    public ArrayList<Mitarbeiter> getPersonal(){
+
+    }
+    public ArrayList<Schueler> getSchueler(){
+
+    }
+    public int getAnzahlSchueler(){
+
+    }
+}
